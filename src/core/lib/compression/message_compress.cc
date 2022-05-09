f695bf4acdf968cd3c5c3860bd72eac27329e06d
zkkxu <goodbodyguard@163.com>
2022-05-09 18:09:56 +0800

    up

--- src/core/lib/compression/message_compress.cc
+++ src/core/lib/compression/message_compress.cc
@@ -457,7 +457,9 @@ static int compress_inner(grpc_compression_algorithm algorithm,
     case GRPC_COMPRESS_GZIP:
       return zlib_compress(input, output, 1);
     case GRPC_COMPRESS_LZ4:
-      return lz4_compress(input, output);
+      if (input->length > 4096)
+        return lz4_compress(input, output);
+      else return 0;
     case GRPC_COMPRESS_ALGORITHMS_COUNT:
       break;
   }
