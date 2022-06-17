def56978b4103ec49a6131057c75d3652cf3f50e
zkkxu <zkkxu@users.noreply.github.com>
2022-06-17 03:30:25 +0000

    Automated change: Fix sanity tests

--- src/core/lib/compression/message_compress.cc
+++ src/core/lib/compression/message_compress.cc
@@ -19,17 +19,21 @@
 #include <grpc/support/port_platform.h>
 
 #include "src/core/lib/compression/message_compress.h"
-#include "src/core/lib/compression/compression_internal.h"
+
+#include <string.h>
 
 #include <memory>
-#include <string.h>
+
 #include <zconf.h>
 #include <zlib.h>
+
+#include "absl/memory/memory.h"
 
 #include <grpc/slice_buffer.h>
 #include <grpc/support/alloc.h>
 #include <grpc/support/log.h>
 
+#include "src/core/lib/compression/compression_internal.h"
 #include "src/core/lib/slice/slice_refcount.h"
 
 namespace grpc_core {
@@ -40,16 +44,16 @@ class CompressionOptionsImpl : public CompressionOptions {
       : gzip_compression_level_(
             DefaultGzipCompressionLevelFromChannelArgs(args)) {}
 
-  explicit CompressionOptionsImpl()
-      : gzip_compression_level_(0) {}
-      
+  explicit CompressionOptionsImpl() : gzip_compression_level_(0) {}
+
   int gzip_compression_level() const { return gzip_compression_level_; }
-  
+
  private:
   int gzip_compression_level_;
 };
 
-std::unique_ptr<CompressionOptions> MakeCompressionOptions(const grpc_channel_args* args) {
+std::unique_ptr<CompressionOptions> MakeCompressionOptions(
+    const grpc_channel_args* args) {
   return absl::make_unique<CompressionOptionsImpl>(args);
 }
 }  // namespace grpc_core
@@ -117,7 +121,8 @@ static void* zalloc_gpr(void* /*opaque*/, unsigned int items,
 static void zfree_gpr(void* /*opaque*/, void* address) { gpr_free(address); }
 
 static int zlib_compress(grpc_slice_buffer* input, grpc_slice_buffer* output,
-                         int gzip, const grpc_core::CompressionOptionsImpl* options) {
+                         int gzip,
+                         const grpc_core::CompressionOptionsImpl* options) {
   z_stream zs;
   int r;
   size_t i;
@@ -126,8 +131,8 @@ static int zlib_compress(grpc_slice_buffer* input, grpc_slice_buffer* output,
   memset(&zs, 0, sizeof(zs));
   zs.zalloc = zalloc_gpr;
   zs.zfree = zfree_gpr;
-  r = deflateInit2(&zs, options->gzip_compression_level(), Z_DEFLATED, 15 | (gzip ? 16 : 0),
-                   8, Z_DEFAULT_STRATEGY);
+  r = deflateInit2(&zs, options->gzip_compression_level(), Z_DEFLATED,
+                   15 | (gzip ? 16 : 0), 8, Z_DEFAULT_STRATEGY);
   GPR_ASSERT(r == Z_OK);
   r = zlib_body(&zs, input, output, deflate) && output->length < input->length;
   if (!r) {
@@ -174,8 +179,7 @@ static int copy(grpc_slice_buffer* input, grpc_slice_buffer* output) {
 }
 
 static int compress_inner(grpc_compression_algorithm algorithm,
-                          grpc_slice_buffer* input, 
-                          grpc_slice_buffer* output,
+                          grpc_slice_buffer* input, grpc_slice_buffer* output,
                           const grpc_core::CompressionOptionsImpl* options) {
   switch (algorithm) {
     case GRPC_COMPRESS_NONE:
@@ -194,11 +198,11 @@ static int compress_inner(grpc_compression_algorithm algorithm,
 }
 
 int grpc_msg_compress(grpc_compression_algorithm algorithm,
-                      grpc_slice_buffer* input, 
-                      grpc_slice_buffer* output,
+                      grpc_slice_buffer* input, grpc_slice_buffer* output,
                       const grpc_core::CompressionOptions* options) {
-  if (!compress_inner(algorithm, input, output, 
-                      static_cast<const grpc_core::CompressionOptionsImpl*>(options)) ) {
+  if (!compress_inner(
+          algorithm, input, output,
+          static_cast<const grpc_core::CompressionOptionsImpl*>(options))) {
     copy(input, output);
     return 0;
   }
